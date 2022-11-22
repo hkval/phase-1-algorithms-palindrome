21da8341d8e2984606c3e6b9fc8e9fa2796d3944
Holly <hollykvalheim@gmail.com>
2022-11-21 19:57:43 -0800

    commit tree b094a0984e6c0dae992ddbbdb14d986763343493

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const array1 = word.split('')
+  return word === reverse(array1).join('') ? true : false
+}  
+  
+function reverse(array){
+  const revArray = []
+  let e = 0
+  let i = array.length - 1
+  while (i >= 0){
+    revArray[i] = array[e]
+    i--
+    e++
+  }
+  return revArray
 }
 
 /* 
   Add your pseudocode here
+  Take input, reverse the order, and check if the reversal matches the input.
+
+  1. Turn the input into an array of letters.
+  2. Count the number of letters.
+  3. Return a new array, with the original letters assigned new indexes.
+      Something like, "For each letter, add it to the new array with index of
+          len(input)" and each loop reduces the index.
 */
 
 /*
-  Add written explanation of your solution here
+I converted the input string into an array, created a new array with the elements
+reversed, and compared the new array (as a string) to the original input.
 */
 
 // You can run `node index.js` to view these console logs
